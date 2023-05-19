d12d9af13c22453dc605243436e8385c2efecb7f
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-19 20:19:06 +0300

    feat(index): user can drop pieces into a grid

---
+++ src/index.py
@@ -0,0 +1,41 @@
+class Connect4Game:
+    def __init__(self):
+        self.game_grid = [['.' for _ in range(7)] for _ in range(6)]  
+        self.running = True 
+
+    def print_grid(self):
+        for row in self.game_grid:
+            print(row)
+    
+    def update_grid(self, column, player):
+        if player == "player":
+            piece = "X"
+        else:
+            piece = "O"
+
+        for row in range(5, -1, -1):
+            if self.game_grid[row][column] == ".":
+                self.game_grid[row][column] = piece
+                break
+            else:
+                continue
+
+    def ask_for_input(self):
+        print("Please enter a column number (1-7): ")
+        column = int(input()) - 1
+        if column < 0 or column > 6:
+            print("Invalid column number, please try again.")
+            self.ask_for_input()
+        if self.game_grid[0][column] != ".":
+            print("Column is full, please choose another.")
+            self.ask_for_input()
+        if column == -1:
+            self.running = False
+        else:
+            self.update_grid(column, "player")
+
+if __name__ == "__main__":
+    game = Connect4Game()
+    while game.running:
+        game.print_grid()
+        game.ask_for_input()
