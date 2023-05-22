1498a221080769451ab693d1bdff188e1ae5bf41
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-05-22 20:41:50 +0300

    refactor: modularize game logic from UI
    Breaking: tests break from this change

--- src/index.py
+++ src/index.py
@@ -1,4 +1,6 @@
-class Connect4Game:
+from services.game_service import game_service
+
+class GameUI:
     def __init__(self):
         """Initializes the game grid and sets the game to running.
 
@@ -14,97 +16,6 @@ class Connect4Game:
         """
         self.game_grid = [['.' for _ in range(7)] for _ in range(6)]  
         self.running = True 
-
-    def print_grid(self):
-        for row in self.game_grid:
-            print(row)
-    
-    def update_grid(self, column, player):
-        """Updates the grid with a piece dropped by a player.
-
-        Args:
-            column(int, 0-6): The column number to drop the piece in.
-            player(bool): True if the piece is dropped by the player,
-                False if dropped by the computer.
-        """
-        column = int(column)
-
-        if player:
-            piece = "X"
-        else:
-            piece = "O"
-
-        for row in range(5, -1, -1):
-            if self.game_grid[row][column] == ".":
-                self.game_grid[row][column] = piece
-                piece_row = row
-                piece_column = column
-                break
-            else:
-                continue
-
-        if self.check_win(piece_row, piece_column):
-            self.print_grid()
-            if player:
-                print("You win!")
-            else:
-                print("You lose!")
-            self.running = False
-
-    def check_win(self, piece_row=5, piece_column=0):
-        """Checks if the game has been won around a piece.
-
-        Args:
-            piece_row(int): The row number of the last piece dropped.
-            piece_column(int): The column number of the last piece dropped.
-
-        Returns:
-            True if the game has been won, False if not.
-        """
-        for row in range(piece_row-3, piece_row+4):
-            if row < 0 or row > 5:
-                continue
-            for column in range(piece_column-3, piece_column+4):
-                if column < 0 or column > 6:
-                    continue
-                if self.game_grid[row][column] != ".":
-                    try:
-                        if (self.game_grid[row][column]
-                            == self.game_grid[row][column+1]
-                            == self.game_grid[row][column+2]
-                            == self.game_grid[row][column+3]
-                        ):
-                            return True
-                    except IndexError:
-                        pass
-                    try:
-                        if (self.game_grid[row][column]
-                            == self.game_grid[row+1][column]
-                            == self.game_grid[row+2][column]
-                            == self.game_grid[row+3][column]
-                        ):
-                            return True
-                    except IndexError:
-                        pass
-                    try:
-                        if (self.game_grid[row][column]
-                            == self.game_grid[row+1][column+1]
-                            == self.game_grid[row+2][column+2]
-                            == self.game_grid[row+3][column+3]
-                        ):
-                            return True
-                    except IndexError:
-                        pass
-                    try:
-                        if (self.game_grid[row][column]
-                            == self.game_grid[row+1][column-1]
-                            == self.game_grid[row+2][column-2]
-                            == self.game_grid[row+3][column-3]
-                        ):
-                            return True
-                    except IndexError:
-                        continue
-        return False
 
     def ask_for_input(self):
         """Prompts the user for a column number to drop a piece in.
@@ -122,7 +33,7 @@ class Connect4Game:
             elif column == -1:
                 self.running = False
             else:
-                self.update_grid(column, True)
+                game_service.update_grid(column, True)
         except ValueError:
             print("Invalid column number, please try again.")
         except IndexError:
@@ -130,7 +41,7 @@ class Connect4Game:
 
 
 if __name__ == "__main__":
-    game = Connect4Game()
-    while game.running:
-        game.print_grid()
-        game.ask_for_input()
+    game_UI = GameUI()
+    while game_service.running:
+        game_service.print_grid()
+        game_UI.ask_for_input()
---
+++ src/services/game_service.py
@@ -0,0 +1,109 @@
+class GameService:
+    def __init__(self):
+        """Initializes the game grid and sets the game to running.
+
+        The game grid is a 2D list of 6 rows and 7 columns. Each empty tile is
+        represented by a character:
+            '.' = empty tile
+            'X' = player piece (human)
+            'O' = computer piece
+
+        Attributes:
+            game_grid(list): A 2D list of 6 rows and 7 columns.
+            running(bool): True if the game is running, False if the game is over.
+        """
+        self.game_grid = [['.' for _ in range(7)] for _ in range(6)]  
+        self.running = True 
+
+    def print_grid(self):
+        for row in self.game_grid:
+            print(row)
+    
+    def update_grid(self, column, player):
+        """Updates the grid with a piece dropped by a player.
+
+        Args:
+            column(int, 0-6): The column number to drop the piece in.
+            player(bool): True if the piece is dropped by the player,
+                False if dropped by the computer.
+        """
+        column = int(column)
+
+        if player:
+            piece = "X"
+        else:
+            piece = "O"
+
+        for row in range(5, -1, -1):
+            if self.game_grid[row][column] == ".":
+                self.game_grid[row][column] = piece
+                piece_row = row
+                piece_column = column
+                break
+            else:
+                continue
+
+        if self.check_win(piece_row, piece_column):
+            self.print_grid()
+            if player:
+                print("You win!")
+            else:
+                print("You lose!")
+            self.running = False
+
+    def check_win(self, piece_row=5, piece_column=0):
+        """Checks if the game has been won around a piece.
+
+        Args:
+            piece_row(int): The row number of the last piece dropped.
+            piece_column(int): The column number of the last piece dropped.
+
+        Returns:
+            True if the game has been won, False if not.
+        """
+        for row in range(piece_row-3, piece_row+4):
+            if row < 0 or row > 5:
+                continue
+            for column in range(piece_column-3, piece_column+4):
+                if column < 0 or column > 6:
+                    continue
+                if self.game_grid[row][column] != ".":
+                    try:
+                        if (self.game_grid[row][column]
+                            == self.game_grid[row][column+1]
+                            == self.game_grid[row][column+2]
+                            == self.game_grid[row][column+3]
+                        ):
+                            return True
+                    except IndexError:
+                        pass
+                    try:
+                        if (self.game_grid[row][column]
+                            == self.game_grid[row+1][column]
+                            == self.game_grid[row+2][column]
+                            == self.game_grid[row+3][column]
+                        ):
+                            return True
+                    except IndexError:
+                        pass
+                    try:
+                        if (self.game_grid[row][column]
+                            == self.game_grid[row+1][column+1]
+                            == self.game_grid[row+2][column+2]
+                            == self.game_grid[row+3][column+3]
+                        ):
+                            return True
+                    except IndexError:
+                        pass
+                    try:
+                        if (self.game_grid[row][column]
+                            == self.game_grid[row+1][column-1]
+                            == self.game_grid[row+2][column-2]
+                            == self.game_grid[row+3][column-3]
+                        ):
+                            return True
+                    except IndexError:
+                        continue
+        return False
+
+game_service = GameService()
