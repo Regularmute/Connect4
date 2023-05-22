71d7cf34a77f22573c5f345ef3afe5a9d0a08b60
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-05-22 20:42:43 +0300

    test(gameserv): update testfile to fit modularization

--- src/tests/services/game_service_test.py
+++ src/tests/services/game_service_test.py
@@ -1,14 +1,13 @@
 import unittest
 import sys
 from io import StringIO
-from index import Connect4Game
+from services.game_service import GameService
 
-class TestConsoleGui(unittest.TestCase):
+class TestGameService(unittest.TestCase):
     def setUp(self):
-        print("Setting up test...")
+        self.game_service = GameService()
 
     def test_initial_grid_is_printed_correctly(self):
-        game = Connect4Game()
         expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
@@ -18,13 +17,11 @@ class TestConsoleGui(unittest.TestCase):
         )
         output = StringIO()
         sys.stdout = output
-        game.print_grid()
+        self.game_service.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
 
     def test_grid_is_printed_correctly_after_one_player_piece(self):
-        game = Connect4Game()
-
         expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
@@ -33,17 +30,15 @@ class TestConsoleGui(unittest.TestCase):
             +"\n['X', '.', '.', '.', '.', '.', '.']\n"
         )
 
-        game.update_grid(0, True)
+        self.game_service.update_grid(0, True)
 
         output = StringIO()
         sys.stdout = output
-        game.print_grid()
+        self.game_service.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
 
     def test_grid_is_printed_correctly_after_two_player_pieces(self):
-        game = Connect4Game()
-
         expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
@@ -52,17 +47,16 @@ class TestConsoleGui(unittest.TestCase):
             +"\n['.', '.', '.', 'X', '.', '.', '.']\n"
         )
 
-        game.update_grid(3, True)
-        game.update_grid(3, True)
+        self.game_service.update_grid(3, True)
+        self.game_service.update_grid(3, True)
 
         output = StringIO()
         sys.stdout = output
-        game.print_grid()
+        self.game_service.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
 
     def test_grid_is_printed_correctly_after_one_computer_piece(self):
-        game = Connect4Game()
 
         expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
@@ -72,16 +66,15 @@ class TestConsoleGui(unittest.TestCase):
             +"\n['.', 'O', '.', '.', '.', '.', '.']\n"
         )
 
-        game.update_grid(1, False)
+        self.game_service.update_grid(1, False)
 
         output = StringIO()
         sys.stdout = output
-        game.print_grid()
+        self.game_service.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
 
     def test_grid_is_printed_correctly_after_two_computer_pieces(self):
-        game = Connect4Game()
 
         expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
@@ -91,17 +84,17 @@ class TestConsoleGui(unittest.TestCase):
             +"\n['.', '.', '.', '.', 'O', '.', '.']\n"
         )
 
-        game.update_grid(4, False)
-        game.update_grid(4, False)
+        self.game_service.update_grid(4, False)
+        self.game_service.update_grid(4, False)
 
         output = StringIO()
         sys.stdout = output
-        game.print_grid()
+        self.game_service.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
 
     def test_grid_is_printed_correctly_after_two_pieces_from_both_sides(self):
-        game = Connect4Game()
+        
 
         expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
             +"\n['.', '.', '.', '.', '.', '.', '.']"
@@ -111,125 +104,125 @@ class TestConsoleGui(unittest.TestCase):
             +"\n['.', '.', 'O', 'X', 'X', '.', '.']\n"
         )
 
-        game.update_grid(3, True)
-        game.update_grid(3, False)
-        game.update_grid(4, True)
-        game.update_grid(2, False)
+        self.game_service.update_grid(3, True)
+        self.game_service.update_grid(3, False)
+        self.game_service.update_grid(4, True)
+        self.game_service.update_grid(2, False)
 
         output = StringIO()
         sys.stdout = output
-        game.print_grid()
+        self.game_service.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
 
     def test_check_win_returns_false_for_empty_grid(self):
-        game = Connect4Game()
+        
 
-        self.assertFalse(game.check_win())
+        self.assertFalse(self.game_service.check_win())
 
     def test_check_win_returns_true_for_horizontal_win(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(0, True)
-        game.update_grid(1, True)
-        game.update_grid(2, True)
-        game.update_grid(3, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(1, True)
+        self.game_service.update_grid(2, True)
+        self.game_service.update_grid(3, True)
 
-        self.assertTrue(game.check_win(5,3))
+        self.assertTrue(self.game_service.check_win(5,3))
 
     def test_check_win_returns_true_for_vertical_win(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(0, True)
-        game.update_grid(0, True)
-        game.update_grid(0, True)
-        game.update_grid(0, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(0, True)
 
-        self.assertTrue(game.check_win(2,0))
+        self.assertTrue(self.game_service.check_win(2,0))
 
     def test_check_win_returns_true_for_rising_diagonal_win(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(0, True)
+        self.game_service.update_grid(0, True)
 
-        game.update_grid(1, False)
-        game.update_grid(1, True)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, True)
 
-        game.update_grid(2, False)
-        game.update_grid(2, False)
-        game.update_grid(2, True)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, True)
 
-        game.update_grid(3, False)
-        game.update_grid(3, False)
-        game.update_grid(3, False)
-        game.update_grid(3, True)
+        self.game_service.update_grid(3, False)
+        self.game_service.update_grid(3, False)
+        self.game_service.update_grid(3, False)
+        self.game_service.update_grid(3, True)
 
-        self.assertTrue(game.check_win(2,3))
+        self.assertTrue(self.game_service.check_win(2,3))
 
     def test_check_win_returns_true_for_falling_diagonal_win(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(1, False)
-        game.update_grid(1, False)
-        game.update_grid(1, False)
-        game.update_grid(1, True)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, True)
 
-        game.update_grid(2, False)
-        game.update_grid(2, False)
-        game.update_grid(2, True)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, True)
 
-        game.update_grid(3, False)
-        game.update_grid(3, True)
+        self.game_service.update_grid(3, False)
+        self.game_service.update_grid(3, True)
 
-        game.update_grid(4, True)
+        self.game_service.update_grid(4, True)
 
-        self.assertTrue(game.check_win(0,4))
+        self.assertTrue(self.game_service.check_win(0,4))
 
     def test_check_win_returns_false_for_three_horizontal_pieces(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(0, True)
-        game.update_grid(1, True)
-        game.update_grid(2, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(1, True)
+        self.game_service.update_grid(2, True)
 
-        self.assertFalse(game.check_win(0,2))
+        self.assertFalse(self.game_service.check_win(0,2))
 
     def test_check_win_returns_false_for_three_vertical_pieces(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(0, True)
-        game.update_grid(0, True)
-        game.update_grid(0, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(0, True)
+        self.game_service.update_grid(0, True)
 
-        self.assertFalse(game.check_win(2,0))
+        self.assertFalse(self.game_service.check_win(2,0))
 
     def test_check_win_returns_false_for_three_rising_diagonal_pieces(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(0, True)
+        self.game_service.update_grid(0, True)
 
-        game.update_grid(1, False)
-        game.update_grid(1, True)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, True)
 
-        game.update_grid(2, False)
-        game.update_grid(2, False)
-        game.update_grid(2, True)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, True)
 
-        self.assertFalse(game.check_win(3,2))
+        self.assertFalse(self.game_service.check_win(3,2))
 
     def test_check_win_returns_false_for_three_falling_diagonal_pieces(self):
-        game = Connect4Game()
+        
 
-        game.update_grid(1, False)
-        game.update_grid(1, False)
-        game.update_grid(1, False)
-        game.update_grid(1, True)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, False)
+        self.game_service.update_grid(1, True)
 
-        game.update_grid(2, False)
-        game.update_grid(2, False)
-        game.update_grid(2, True)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, False)
+        self.game_service.update_grid(2, True)
 
-        game.update_grid(3, False)
-        game.update_grid(3, True)
+        self.game_service.update_grid(3, False)
+        self.game_service.update_grid(3, True)
 
-        self.assertFalse(game.check_win(4,3))
+        self.assertFalse(self.game_service.check_win(4,3))
