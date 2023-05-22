c269c6b6108982275735ab662318f745274c5d40
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-22 15:20:26 +0300

    docs(index.py): add docstring

--- src/index.py
+++ src/index.py
@@ -1,5 +1,17 @@
 class Connect4Game:
     def __init__(self):
+        """Initializes the game grid and sets the game to running.
+
+        The game grid is a 2D list of 6 rows and 7 columns. Each empty tile is
+        represented by a character:
+            '.' = empty tile
+            'X' = player piece (human)
+            'O' = computer piece
+
+        Attributes:
+            game_grid(list): A 2D list of 6 rows and 7 columns.
+            running(bool): True if the game is running, False if the game is over.
+        """
         self.game_grid = [['.' for _ in range(7)] for _ in range(6)]  
         self.running = True 
 
@@ -8,6 +20,15 @@ class Connect4Game:
             print(row)
     
     def update_grid(self, column, player):
+        """Updates the grid with a piece dropped by a player.
+
+        Args:
+            column(int, 0-6): The column number to drop the piece in.
+            player(bool): True if the piece is dropped by the player,
+                False if dropped by the computer.
+        """
+        column = int(column)
+
         if player:
             piece = "X"
         else:
@@ -21,6 +42,13 @@ class Connect4Game:
                 continue
 
     def ask_for_input(self):
+        """Prompts the user for a column number to drop a piece in.
+
+        Calls update_grid() if the input is valid.
+        Exceptions:
+            ValueError: If the input is not an integer.
+            IndexError: If the input is not between 1-7.
+        """
         print("Please enter a column number (1-7): ")
         try:
             column = int(input()) - 1
