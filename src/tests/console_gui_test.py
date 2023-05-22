0935e48ba1a705871c076b07ca9dea09ad59d2d6
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-22 15:05:23 +0300

    test(updategrid): add tests for moves

--- src/tests/console_gui_test.py
+++ src/tests/console_gui_test.py
@@ -21,3 +21,103 @@ class TestConsoleGui(unittest.TestCase):
         game.print_grid()
         sys.stdout = sys.__stdout__
         self.assertEqual(output.getvalue(), expected_output)
+
+    def test_grid_is_printed_correctly_after_one_player_piece(self):
+        game = Connect4Game()
+
+        expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['X', '.', '.', '.', '.', '.', '.']\n"
+        )
+
+        game.update_grid(0, True)
+
+        output = StringIO()
+        sys.stdout = output
+        game.print_grid()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), expected_output)
+
+    def test_grid_is_printed_correctly_after_two_player_pieces(self):
+        game = Connect4Game()
+
+        expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', 'X', '.', '.', '.']"
+            +"\n['.', '.', '.', 'X', '.', '.', '.']\n"
+        )
+
+        game.update_grid(3, True)
+        game.update_grid(3, True)
+
+        output = StringIO()
+        sys.stdout = output
+        game.print_grid()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), expected_output)
+
+    def test_grid_is_printed_correctly_after_one_computer_piece(self):
+        game = Connect4Game()
+
+        expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', 'O', '.', '.', '.', '.', '.']\n"
+        )
+
+        game.update_grid(1, False)
+
+        output = StringIO()
+        sys.stdout = output
+        game.print_grid()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), expected_output)
+
+    def test_grid_is_printed_correctly_after_two_computer_pieces(self):
+        game = Connect4Game()
+
+        expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', 'O', '.', '.']"
+            +"\n['.', '.', '.', '.', 'O', '.', '.']\n"
+        )
+
+        game.update_grid(4, False)
+        game.update_grid(4, False)
+
+        output = StringIO()
+        sys.stdout = output
+        game.print_grid()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), expected_output)
+
+    def test_grid_is_printed_correctly_after_two_pieces_from_both_sides(self):
+        game = Connect4Game()
+
+        expected_output = ("['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', '.', '.', '.', '.']"
+            +"\n['.', '.', '.', 'O', '.', '.', '.']"
+            +"\n['.', '.', 'O', 'X', 'X', '.', '.']\n"
+        )
+
+        game.update_grid(3, True)
+        game.update_grid(3, False)
+        game.update_grid(4, True)
+        game.update_grid(2, False)
+
+        output = StringIO()
+        sys.stdout = output
+        game.print_grid()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(output.getvalue(), expected_output)
