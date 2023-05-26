2afdbc02cba3a57eaefd6347b3c88a6d90e60ca7
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-26 13:13:45 +0300

    docs(AI): add docstring

--- src/services/ai_service.py
+++ src/services/ai_service.py
@@ -1,13 +1,21 @@
 import random
 
 class Connect4AI:
-    """Artificial intelligence for the game."""
+    """Artificial intelligence for the game.
+
+    Attributes:
+        game_service(GameService): The game logic with the game's information.
+    """
 
     def __init__(self, game_service):
         self.game_service = game_service
 
     def choose_column(self):
-        """Chooses a random column to drop a piece in."""
+        """Chooses a random column to drop a piece in.
+
+        Returns:
+            column(int): The column number to drop the piece in.
+        """
         column = random.randint(0, 6)
         while self.game_service.game_grid[0][column] != ".":
             column = random.randint(0, 6)
