2a64f81878d3ce0ebe29b970cac2390213e2cb33
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-05-29 16:39:03 +0300

    refactor(index): call victory checking inside main loop

--- src/index.py
+++ src/index.py
@@ -10,10 +10,16 @@ if __name__ == "__main__":
         game_UI.print_grid()
         column = game_UI.get_column_from_player()
         game_service.update_grid(game_service.game_grid, column, True)
+        if game_service.check_win_including_piece(game_service.game_grid, column):
+            print("Player won!")
+            game_service.running = False
         if not game_service.running:
             break
         column = ai_service.choose_column_minimax()
         game_service.update_grid(game_service.game_grid, column, False)
+        if game_service.check_win_including_piece(game_service.game_grid, column):
+            print("Computer won!")
+            game_service.running = False
     print("Board at the end:")
     print("#######################################")
     game_UI.print_grid()
