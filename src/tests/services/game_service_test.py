a29672ea337049e0a4fdcf48bae3ec21c435c68b
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-05-29 15:27:37 +0300

    test(gameserv): vitory checking on right side of grid

--- src/tests/services/game_service_test.py
+++ src/tests/services/game_service_test.py
@@ -16,6 +16,14 @@ class TestGameService(unittest.TestCase):
 
         self.assertTrue(self.game_service.check_win_including_piece(5,3))
 
+    def test_check_win_including_piece_returns_true_for_horizontal_win_2(self):
+        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_service.update_grid(self.game_service.game_grid, 4, True)
+        self.game_service.update_grid(self.game_service.game_grid, 5, True)
+        self.game_service.update_grid(self.game_service.game_grid, 6, True)
+
+        self.assertTrue(self.game_service.check_win_including_piece(5,6))
+
     def test_check_win_including_piece_returns_true_for_vertical_win(self):
         self.game_service.update_grid(self.game_service.game_grid, 0, True)
         self.game_service.update_grid(self.game_service.game_grid, 0, True)
@@ -41,6 +49,24 @@ class TestGameService(unittest.TestCase):
 
         self.assertTrue(self.game_service.check_win_including_piece(2,3))
 
+    def test_check_win_including_piece_returns_true_for_rising_diagonal_win_2(self):
+        # Checks for rising diagonal win on the right side of the grid
+        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+
+        self.game_service.update_grid(self.game_service.game_grid, 4, False)
+        self.game_service.update_grid(self.game_service.game_grid, 4, True)
+
+        self.game_service.update_grid(self.game_service.game_grid, 5, False)
+        self.game_service.update_grid(self.game_service.game_grid, 5, False)
+        self.game_service.update_grid(self.game_service.game_grid, 5, True)
+
+        self.game_service.update_grid(self.game_service.game_grid, 6, False)
+        self.game_service.update_grid(self.game_service.game_grid, 6, False)
+        self.game_service.update_grid(self.game_service.game_grid, 6, False)
+        self.game_service.update_grid(self.game_service.game_grid, 6, True)
+
+        self.assertTrue(self.game_service.check_win_including_piece(2,6))
+
     def test_check_win_including_piece_returns_true_for_falling_diagonal_win(self):
         self.game_service.update_grid(self.game_service.game_grid, 1, False)
         self.game_service.update_grid(self.game_service.game_grid, 1, False)
@@ -57,6 +83,24 @@ class TestGameService(unittest.TestCase):
         self.game_service.update_grid(self.game_service.game_grid, 4, True)
 
         self.assertTrue(self.game_service.check_win_including_piece(5,4))
+
+    def test_check_win_including_piece_returns_true_for_falling_diagonal_win_2(self):
+        # Checks for falling diagonal wins on the right side of the board
+        self.game_service.update_grid(self.game_service.game_grid, 3, False)
+        self.game_service.update_grid(self.game_service.game_grid, 3, False)
+        self.game_service.update_grid(self.game_service.game_grid, 3, False)
+        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+
+        self.game_service.update_grid(self.game_service.game_grid, 4, False)
+        self.game_service.update_grid(self.game_service.game_grid, 4, False)
+        self.game_service.update_grid(self.game_service.game_grid, 4, True)
+
+        self.game_service.update_grid(self.game_service.game_grid, 5, False)
+        self.game_service.update_grid(self.game_service.game_grid, 5, True)
+
+        self.game_service.update_grid(self.game_service.game_grid, 6, True)
+
+        self.assertTrue(self.game_service.check_win_including_piece(5,6))
 
     def test_check_win_including_piece_returns_false_for_three_horizontal_pieces(self):
         self.game_service.update_grid(self.game_service.game_grid, 0, True)
