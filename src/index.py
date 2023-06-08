a5ca966f2e9b351f170008f76a1383469eda8455
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-06-08 14:32:12 +0300

    fix(index): now properly calls game_ai for methods

--- src/index.py
+++ src/index.py
@@ -1,25 +1,23 @@
-from services.game_service import GameService
-from services.ai_service import Connect4AI
+from ai.game_ai import GameAI
 from ui.game_ui import GameUI
 
 if __name__ == "__main__":
-    game_service = GameService()
-    ai_service = Connect4AI(game_service)
-    game_UI = GameUI(game_service)
-    while game_service.running:
+    game_ai = GameAI()
+    game_UI = GameUI(game_ai)
+    while game_ai.running:
         game_UI.print_grid()
         column = game_UI.get_column_from_player()
-        game_service.update_grid(game_service.game_grid, column, True)
-        if game_service.check_win_including_piece(game_service.game_grid, column):
+        game_ai.update_grid(game_ai.game_grid, column, True)
+        if game_ai.check_win_including_piece(game_ai.game_grid, column):
             print("Player won!")
-            game_service.running = False
-        if not game_service.running:
+            game_ai.running = False
+        if not game_ai.running:
             break
-        column = ai_service.choose_column_minimax()
-        game_service.update_grid(game_service.game_grid, column, False)
-        if game_service.check_win_including_piece(game_service.game_grid, column):
+        column = game_ai.choose_column_minimax()
+        game_ai.update_grid(game_ai.game_grid, column, False)
+        if game_ai.check_win_including_piece(game_ai.game_grid, column):
             print("Computer won!")
-            game_service.running = False
+            game_ai.running = False
     print("Board at the end:")
     print("#######################################")
     game_UI.print_grid()
