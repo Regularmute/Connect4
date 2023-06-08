9960ebbd1e4c8be8517557f899d07334f8359133
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-06-08 14:17:01 +0300

    test(gameServ): BREAKS TESTS, set up tests for refactoring
    The GameSErvice Class will be combined with the
    AIService Class into one game_ai class. This commit
    breaks current tests in preparation for the large-
    scale project restructuring.

--- src/tests/services/game_service_test.py
+++ src/tests/services/game_service_test.py
@@ -1,144 +1,144 @@
 import unittest
-from services.game_service import GameService
+from ai.game_ai import GameAI
 
-class TestGameService(unittest.TestCase):
+class TestGameAI(unittest.TestCase):
     def setUp(self):
-        self.game_service = GameService()
+        self.game_ai = GameAI()
 
     def test_check_win_including_piece_returns_false_for_empty_grid(self):
-        self.assertFalse(self.game_service.check_win_including_piece(self.game_service.game_grid, 0))
+        self.assertFalse(self.game_ai.check_win_including_piece(self.game_ai.game_grid, 0))
 
     def test_check_win_including_piece_returns_true_for_horizontal_win(self):
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 1, True)
-        self.game_service.update_grid(self.game_service.game_grid, 2, True)
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,3))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,3))
 
     def test_check_win_including_piece_returns_true_for_horizontal_win_2(self):
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
-        self.game_service.update_grid(self.game_service.game_grid, 4, True)
-        self.game_service.update_grid(self.game_service.game_grid, 5, True)
-        self.game_service.update_grid(self.game_service.game_grid, 6, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 5, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 6, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,6))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,6))
 
     def test_check_win_including_piece_returns_true_for_vertical_win(self):
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,0))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,0))
 
     def test_check_win_including_piece_returns_true_for_rising_diagonal_win(self):
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,3))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,3))
 
     def test_check_win_including_piece_returns_true_for_rising_diagonal_win_2(self):
         # Checks for rising diagonal win on the right side of the grid
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 4, False)
-        self.game_service.update_grid(self.game_service.game_grid, 4, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 5, False)
-        self.game_service.update_grid(self.game_service.game_grid, 5, False)
-        self.game_service.update_grid(self.game_service.game_grid, 5, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 5, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 5, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 5, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 6, False)
-        self.game_service.update_grid(self.game_service.game_grid, 6, False)
-        self.game_service.update_grid(self.game_service.game_grid, 6, False)
-        self.game_service.update_grid(self.game_service.game_grid, 6, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 6, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 6, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 6, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 6, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,6))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,6))
 
     def test_check_win_including_piece_returns_true_for_falling_diagonal_win(self):
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 4, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,4))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,4))
 
     def test_check_win_including_piece_returns_true_for_falling_diagonal_win_2(self):
         # Checks for falling diagonal wins on the right side of the board
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 4, False)
-        self.game_service.update_grid(self.game_service.game_grid, 4, False)
-        self.game_service.update_grid(self.game_service.game_grid, 4, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 4, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 5, False)
-        self.game_service.update_grid(self.game_service.game_grid, 5, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 5, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 5, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 6, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 6, True)
 
-        self.assertTrue(self.game_service.check_win_including_piece(self.game_service.game_grid,6))
+        self.assertTrue(self.game_ai.check_win_including_piece(self.game_ai.game_grid,6))
 
     def test_check_win_including_piece_returns_false_for_three_horizontal_pieces(self):
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 1, True)
-        self.game_service.update_grid(self.game_service.game_grid, 2, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, True)
 
-        self.assertFalse(self.game_service.check_win_including_piece(self.game_service.game_grid,2))
+        self.assertFalse(self.game_ai.check_win_including_piece(self.game_ai.game_grid,2))
 
     def test_check_win_including_piece_returns_false_for_three_vertical_pieces(self):
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
 
-        self.assertFalse(self.game_service.check_win_including_piece(self.game_service.game_grid,0))
+        self.assertFalse(self.game_ai.check_win_including_piece(self.game_ai.game_grid,0))
 
     def test_check_win_including_piece_returns_false_for_three_rising_diagonal_pieces(self):
-        self.game_service.update_grid(self.game_service.game_grid, 0, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 0, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, True)
 
-        self.assertFalse(self.game_service.check_win_including_piece(self.game_service.game_grid,2))
+        self.assertFalse(self.game_ai.check_win_including_piece(self.game_ai.game_grid,2))
 
     def test_check_win_including_piece_returns_false_for_three_falling_diagonal_pieces(self):
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, False)
-        self.game_service.update_grid(self.game_service.game_grid, 1, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 1, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, False)
-        self.game_service.update_grid(self.game_service.game_grid, 2, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 2, True)
 
-        self.game_service.update_grid(self.game_service.game_grid, 3, False)
-        self.game_service.update_grid(self.game_service.game_grid, 3, True)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, False)
+        self.game_ai.update_grid(self.game_ai.game_grid, 3, True)
 
-        self.assertFalse(self.game_service.check_win_including_piece(self.game_service.game_grid,3))
+        self.assertFalse(self.game_ai.check_win_including_piece(self.game_ai.game_grid,3))
