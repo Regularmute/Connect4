d6dda78f2e669ebf691ec58a2bd941f63fb74d4b
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-06-30 19:25:25 +0300

    refactor: rename variables

--- src/ui/game_ui.py
+++ src/ui/game_ui.py
@@ -1,11 +1,11 @@
 class GameUI:
     """User interface for the game."""
-    def __init__(self, game_service):
-        self.game_service = game_service
+    def __init__(self, game_ai):
+        self.game_ai = game_ai
 
     def print_grid(self):
         """Prints the game grid stored in the game logic to the console."""
-        for row in self.game_service.game_grid:
+        for row in self.game_ai.game_grid:
             print(row)
 
     def get_column_from_player(self):
@@ -15,7 +15,7 @@ class GameUI:
                 column = int(input("Enter a column number (1-7): ")) - 1
                 if column < 0 or column > 6:
                     raise ValueError
-                if self.game_service.game_grid[0][column] != 0:
+                if self.game_ai.game_grid[0][column] != 0:
                     print("Column is full, please choose another.")
                     continue
                 return column
